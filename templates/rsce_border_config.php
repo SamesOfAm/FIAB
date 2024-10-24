fd00419738f16799ccebded2a5875859c5fc35c9
SamesOfAm <flo.tepelmann@ewetel.net>
2024-10-24 12:29:29 +0200

    Trennlinie

---
+++ templates/rsce_border_config.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+    'label' => array('Trennlinie', 'Eine horizontale Trennlinie zwischen zwei Elementen'),
+    'types' => array('content', 'module'),
+    'contentCategory' => 'texts',
+    'moduleCategory' => 'miscellaneous',
+    'beTemplate' => 'be_wildcard',
+    'standardFields' => array('cssID'),
+    'wrapper' => array(
+        'type' => 'none',
+    ),
+    'fields' => array(
+
+    )
+
+);
